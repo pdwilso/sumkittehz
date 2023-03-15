2dca603ee351833ee00a14a0cdb3f0f5bcb71449
sum kitteh <pdwilso@gmail.com>
2023-03-15 17:32:31 -0400

    Add files via upload

---
+++ neon-glow-text.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <title>neon glow text</title>
+      <meta charset="UTF-8">
+      <style>
+        h1.neonred {
+          text-shadow: 
+            1px 1px 3px red, -1px -1px 3px red,
+            5px 5px 10px #FF0000, -5px -5px 10px #FF0000;
+        }
+        h1.neongrn {
+          text-shadow: 
+            1px 1px 3px #00AA00, -1px -1px 3px #00AA00,
+            5px 5px 10px #00FF00, -5px -5px 10px #00FF00;
+        }
+     </style>
+  </head>
+  <body>
+    <h1 class="neonred">Text-shadow with red neon glow!</h1>
+    <h1 class="neongrn">Text-shadowed with green neon glow.</h1>
+  </body>
+</html>
